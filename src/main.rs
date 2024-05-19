ddf4ad5789c66bc0ae45c4a455821a2fff2e6d05
Raman Preet Singh <ramanpre0810@gmail.com>
2024-05-19 18:54:49 +0530

    user agent exercise

--- src/main.rs
+++ src/main.rs
@@ -18,19 +18,29 @@ fn main() {
                     .take_while(|x| !x.is_empty())
                     .collect_vec();
 
-                println!("{:?}", lines);
-
                 let path = &lines[0].split(" ").collect_vec()[1].trim();
-                println!("Path: {:?}", path);
 
                 if path == &"/" {
                     s.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("Write failed");
                     continue;
                 }
 
-                if path.starts_with("/echo/") {
+                if path.starts_with("/echo") {
                     let tokens = path.replace("/echo/", "");
                     let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", tokens.len(), tokens.as_str());
+                    s.write(response.as_bytes()).expect("Write failed");
+                    continue;
+                }
+
+                if path.starts_with("/user-agent") {
+                    let user_agent = lines
+                        .iter()
+                        .filter(|x| x.starts_with("User-Agent: "))
+                        .map(|x| x.replace("User-Agent: ", ""))
+                        .next()
+                        .expect("User agent not found");
+
+                    let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", user_agent.len(), user_agent.as_str());
                     s.write(response.as_bytes()).expect("Write failed");
                     continue;
                 }
