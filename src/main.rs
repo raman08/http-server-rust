8784f47793d4abdfef7be92b548358453dec0e8e
Raman Preet Singh <ramanpre0810@gmail.com>
2024-05-19 17:44:54 +0530

    Write stream

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
-// Uncomment this block to pass the first stage
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut s) => {
                 println!("accepted new connection");
+                s.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("Write failed");
             }
             Err(e) => {
                 println!("error: {}", e);
