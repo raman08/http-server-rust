86f0e688ff6a9ebceebaf29a3c0fcdfb99097eac
Raman Preet Singh <ramanpre0810@gmail.com>
2024-05-19 19:03:54 +0530

    multi thread

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,58 @@
 use std::io::{BufRead, BufReader, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::os::unix::thread;
 
 use itertools::Itertools;
+
+fn handle_request(mut stream: TcpStream) {
+    let buf = BufReader::new(stream.try_clone().unwrap());
+    let lines = buf
+        .lines()
+        .map(|line| line.unwrap())
+        .take_while(|x| !x.is_empty())
+        .collect_vec();
+
+    let path = &lines[0].split(" ").collect_vec()[1].trim();
+
+    if path == &"/" {
+        stream
+            .write(b"HTTP/1.1 200 OK\r\n\r\n")
+            .expect("Write failed");
+        return;
+    }
+
+    if path.starts_with("/echo") {
+        let tokens = path.replace("/echo/", "");
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+            tokens.len(),
+            tokens.as_str()
+        );
+        stream.write(response.as_bytes()).expect("Write failed");
+        return;
+    }
+
+    if path.starts_with("/user-agent") {
+        let user_agent = lines
+            .iter()
+            .filter(|x| x.starts_with("User-Agent: "))
+            .map(|x| x.replace("User-Agent: ", ""))
+            .next()
+            .expect("User agent not found");
+
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+            user_agent.len(),
+            user_agent.as_str()
+        );
+        stream.write(response.as_bytes()).expect("Write failed");
+        return;
+    }
+
+    stream
+        .write(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        .expect("Write failed");
+}
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -10,43 +61,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut s) => {
-                let buf = BufReader::new(s.try_clone().unwrap());
-                let lines = buf
-                    .lines()
-                    .map(|line| line.unwrap())
-                    .take_while(|x| !x.is_empty())
-                    .collect_vec();
-
-                let path = &lines[0].split(" ").collect_vec()[1].trim();
-
-                if path == &"/" {
-                    s.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("Write failed");
-                    continue;
-                }
-
-                if path.starts_with("/echo") {
-                    let tokens = path.replace("/echo/", "");
-                    let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", tokens.len(), tokens.as_str());
-                    s.write(response.as_bytes()).expect("Write failed");
-                    continue;
-                }
-
-                if path.starts_with("/user-agent") {
-                    let user_agent = lines
-                        .iter()
-                        .filter(|x| x.starts_with("User-Agent: "))
-                        .map(|x| x.replace("User-Agent: ", ""))
-                        .next()
-                        .expect("User agent not found");
-
-                    let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", user_agent.len(), user_agent.as_str());
-                    s.write(response.as_bytes()).expect("Write failed");
-                    continue;
-                }
-
-                s.write(b"HTTP/1.1 404 Not Found\r\n\r\n")
-                    .expect("Write failed");
+            Ok(s) => {
+                println!("Received Request {:?}", s);
+                std::thread::spawn(|| handle_request(s));
             }
             Err(e) => {
                 println!("error: {}", e);
